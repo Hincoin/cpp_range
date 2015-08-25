7b98efab699dae982896df7feebad3c54ebc31be
Hincoin <lucenadeveloper@gmail.com>
2015-08-25 11:41:39 -0400

    Added 2nd template parameter type for Range
    Changing Range to be a class template of two types, Range<T,U>, instead of Range<T> allows for the step parameter to be an arbitrary type independent of T. Ranges of iterators or pointers should be added to with ints, not another pointer or iterator, for example.

--- include/range.h
+++ include/range.h
@@ -1,75 +1,78 @@
 #ifndef WHOSHUU_Range_H
 #define WHOSHUU_Range_H
 
+#include <iterator>
 #include <exception>
 
 
 namespace whoshuu {
-namespace detail {
+	namespace detail {
 
-template <typename T>
-class Range {
-  public:
-    Range(const T& start, const T& stop, const T& step) : start_{start}, stop_{stop}, step_{step} {
-        if (step_ == 0) {
-            throw std::invalid_argument("Range step argument must not be zero");
-        } else {
-            auto difference = stop_ - start_;
-            if ((difference < 0 && step_ > 0) || (difference > 0 && step_ < 0)) {
-                throw std::invalid_argument("Range arguments must result in termination");
-            }
-        }
-    }
-    
-    class iterator {
-      public:
-        typedef std::forward_iterator_tag iterator_category;
-        typedef T value_type;
-        typedef T& reference;
-        typedef T* pointer;
+		template <typename T,typename U>
+		class Range {
+		public:
+			Range(const T& start, const T& stop, const U& step) : start_{ start }, stop_{ stop }, step_{ step } {
+				if (step_ == 0) {
+					throw std::invalid_argument("Range step argument must not be zero");
+				}
+				else {
+					auto difference = stop_ - start_;
+					if ((difference < 0 && step_ > 0) || (difference > 0 && step_ < 0)) {
+						throw std::invalid_argument("Range arguments must result in termination");
+					}
+				}
+			}
 
-        iterator(value_type value, value_type step) : value_{value}, step_{step} {}
-        iterator operator++() { value_ += step_; return *this; }
-        reference operator*() { return value_; }
-        const pointer operator->() { return &value_; }
-        bool operator==(const iterator& rhs) { return value_ >= rhs.value_; }
-        bool operator!=(const iterator& rhs) { return value_ < rhs.value_; }
-        
-      private:
-        const T step_;
-        value_type value_;
-    };
-    
-    iterator begin() const {
-        return iterator(start_, step_);
-    }
-    
-    iterator end() const {
-        return iterator(stop_, step_);
-    }
-  
-  private:
-    const T start_;
-    const T stop_;
-    const T step_;
-};
+			class iterator {
+			public:
+				typedef std::forward_iterator_tag iterator_category;
+				typedef T value_type_T;
+				typedef U value_type_U;
+				typedef T& reference;
+				typedef T* pointer;
 
-} // namespace detail
+				iterator(value_type_T value, value_type_U step) : value_{ value }, step_{ step } {}
+				iterator operator++() { value_ += step_; return *this; }
+				reference operator*() { return value_; }
+				const pointer operator->() { return &value_; }
+				bool operator==(const iterator& rhs) { return value_ >= rhs.value_; }
+				bool operator!=(const iterator& rhs) { return value_ < rhs.value_; }
 
-template <typename T>
-detail::Range<T> range(const T& stop) {
-    return detail::Range<T>(T{0}, stop, T{1});
-}
+			private:
+				const U step_;
+				value_type_T value_;
+			};
 
-template <typename T>
-detail::Range<T> range(const T& start, const T& stop) {
-    return detail::Range<T>(start, stop, T{1});
-}
+			iterator begin() const {
+				return iterator(start_, step_);
+			}
 
-template <typename T>
-detail::Range<T> range(const T& start, const T& stop, const T& step) {
-    return detail::Range<T>(start, stop, step);
-}
+			iterator end() const {
+				return iterator(stop_, step_);
+			}
+
+		private:
+			const T start_;
+			const T stop_;
+			const U step_;
+		};
+
+	} // namespace detail
+
+	template <typename T>
+	detail::Range<T,T> range(const T& stop) {
+		return detail::Range<T,T>(T{ 0 }, stop, T{ 1 });
+	}
+
+	template <typename T>
+	detail::Range<T,T> range(const T& start, const T& stop) {
+		return detail::Range<T,T>(start, stop, T{ 1 });
+	}
+
+	template <typename T,typename U>
+	detail::Range<T,U> range(const T& start, const T& stop, const U& step) {
+		return detail::Range<T,U>(start, stop, step);
+	}
 
 } // namespace whoshuu
 
